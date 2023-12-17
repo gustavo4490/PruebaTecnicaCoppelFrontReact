abd0e651a51a770fdf80581e80db7c27d3760dad
Andres <andresf150@gmail.com>
2023-12-17 16:51:11 -0600

    New componente header y configuracion

--- coppel/src/App.js
+++ coppel/src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Trabajadores_, Error } from "./paginas/GestorPaginas";
+import Header from "./componenetes/Header";
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Navigation />
       <Routes>
         <Route index element={<Trabajadores_ />} />
         <Route path="/index" element={<Trabajadores_ />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function Navigation() {
+  return (
+    <Header/>
   );
 }
 
--- coppel/src/paginas/PaginaError.jsx
+++ coppel/src/paginas/PaginaError.jsx
@@ -19,7 +19,7 @@ export default function PaginaError() {
                                 to="/index"
                                 className="rounded-md bg-slate-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                Go back home
+                                Regresar
                             </Link>
                         </div>
                     </div>
