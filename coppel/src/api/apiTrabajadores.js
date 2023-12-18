eda672e9ef859255873bb52120b453b3ee52fbc7
Andres <andresf150@gmail.com>
2023-12-17 21:00:38 -0600

    🔨 [Get] obtener todos los trabajadores

--- coppel/src/api/apiTrabajadores.js
+++ coppel/src/api/apiTrabajadores.js
@@ -6,3 +6,9 @@ export const controlApi = axios.create({
       })
 
 export const createTrabajador = (trabajador) => controlApi.post('/trabajadores', trabajador);
+
+export const getAllTrabajadoresPorPagina = async (page) => {
+  return await controlApi.get(`/trabajadores?page=${page}`).then(
+    rest => rest.data
+  );
+};
--- coppel/src/paginas/MostrarTodosLosTrabajadores.jsx
+++ coppel/src/paginas/MostrarTodosLosTrabajadores.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { getAllTrabajadoresPorPagina } from '../api/apiTrabajadores';
+import { useQuery } from 'react-query';
+
+
 
 export default function MostrarTodosLosTrabajadores() {
+  const page = 1;
+
+  const { data, isError, error, isLoading } = useQuery(['getTodosLosTrabajadores', page], () => getAllTrabajadoresPorPagina(page));
+  if (isError) {
+    return <span>Error {error.message}</span>
+  }
+
+  if (isLoading) {
+    // return <GoogleStyleLoading />;
+    return <span>cargando...............</span>;
+
+  }
+
+  // Asumiendo que data es un array de objetos y cada objeto tiene propiedades 'idCapital', 'nombre' y 'saldo'
+  const datosTrabajadores = data.map((element) => {
+    const { nombreCompleto, numeroEmpleado, rol,bonoPorHora,sueldoPorHora,valesDespensa } = element;
+    return { nombreCompleto, numeroEmpleado, rol,bonoPorHora,sueldoPorHora,valesDespensa };
+  });
+  
+  console.log(datosTrabajadores);
   return (
     <div>MostrarTodosLosTrabajadores</div>
   )
